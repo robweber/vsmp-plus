822bc14d90768d221fd25891c3b090f68026dbcd
Rob Weber <robweberjr@gmail.com>
2020-10-09 15:50:50 -0500

    added set python3 as default

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,10 @@
 sudo apt-get install ffmpeg python3-dev python3-rpi.gpio python3-pil python3-numpy python3-pip libopenjp2-7 libtiff5 samba samba-common-bin ffmpeg-python
+
+#set python3 as default
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
+sudo update-alternatives --config python
+
 sudo pip3 install spidev
 sudo pip3 install image
 sudo pip4 install ffmpeg-python
