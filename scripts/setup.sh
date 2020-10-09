3794435677ad3eb5340b9f2222e786ffa09d0944
Rob Weber <robweberjr@gmail.com>
2020-10-09 15:55:37 -0500

    added waveshare library install files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,11 @@
 sudo apt-get install ffmpeg python3-dev python3-rpi.gpio python3-pil python3-numpy python3-pip libopenjp2-7 libtiff5 samba samba-common-bin ffmpeg-python
 
-#set python3 as default
+# setup the waveshare library
+cd ../waveshare_lib
+sudo python setup.py install
+cd ../scripts
+
+# set python3 as default
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
 sudo update-alternatives --config python
