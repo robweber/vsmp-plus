bcf1a58b9aac5fcd104793a268d3ac4f21d78ac0
Rob Weber <robweberjr@gmail.com>
2020-10-23 11:24:33 -0500

    used flake8 to make pep8 compatible

--- vsmp.py
+++ vsmp.py
@@ -1,17 +1,29 @@
-import argparse, ffmpeg, logging, os, time, sys, random, utils
+import argparse
+import ffmpeg
+import logging
+import os
+import utils
 from PIL import Image, ImageDraw, ImageFont
-from waveshare_epd import epd7in5_V2 # ensure this is the correct import for your screen
+from waveshare_epd import epd7in5_V2  # ensure this is the correct import for your screen
 
 # set path to ffmpeg
 os.environ['PATH'] += os.pathsep + '/usr/local/bin/'
 
+
 def generate_frame(in_filename, out_filename, time, width, height):
-    ffmpeg.input(in_filename, ss=time).filter('scale', width, height, force_original_aspect_ratio=1).filter('pad', width, height, -1, -1).output(out_filename, vframes=1).overwrite_output().run(capture_stdout=True, capture_stderr=True)
+    ffmpeg.input(in_filename, ss=time) \
+          .filter('scale', width, height, force_original_aspect_ratio=1) \
+          .filter('pad', width, height, -1, -1) \
+          .output(out_filename, vframes=1) \
+          .overwrite_output() \
+          .run(capture_stdout=True, capture_stderr=True)
+
 
 def check_dir(value):
     if(not os.path.exists(value) and not os.path.isdir(value)):
         raise argparse.ArgumentTypeError("%s is not a directory" % value)
     return value
+
 
 def find_video(args, lastPlayed):
     result = None
@@ -20,7 +32,7 @@ def find_video(args, lastPlayed):
     if(args.file is not None):
         result = args.file
     else:
-        #we're in dir mode, use the name of the last played file
+        # we're in dir mode, use the name of the last played file
         if(lastPlayed != ''):
             result = lastPlayed
         else:
@@ -28,8 +40,9 @@ def find_video(args, lastPlayed):
 
     return result
 
+
 def find_next_video(dir, lastPlayed):
-    #list all files in the directory
+    # list all files in the directory
     fileList = os.listdir(dir)
 
     index = 0  # assume we'll just use the first video
@@ -37,7 +50,7 @@ def find_next_video(dir, lastPlayed):
         for aFile in fileList:
             index = index + 1
             if(aFile == os.path.basename(lastPlayed)):
-                break;
+                break
 
     # go back to start of list if we got to the end
     if(index >= len(fileList)):
@@ -51,20 +64,20 @@ def find_next_video(dir, lastPlayed):
 parser = argparse.ArgumentParser(description='VSMP Settings')
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-f', '--file', type=utils.check_mp4,
-    help="File to grab screens of")
+                   help="File to grab screens of")
 group.add_argument('-d', '--dir', type=check_dir,
-    help="Dir to play videos from (in order)")
+                   help="Dir to play videos from (in order)")
 parser.add_argument('-i', '--increment',  default=4,
-    help="Number of frames skipped between screen updates")
+                    help="Number of frames skipped between screen updates")
 parser.add_argument('-s', '--start', default=1,
-    help="Start at a specific frame")
+                    help="Start at a specific frame")
 parser.add_argument('-t', '--timecode', action='store_true',
-    help='show the video timecode on the bottom of the display')
+                    help='show the video timecode on the bottom of the display')
 
 args = parser.parse_args()
 
 # setup some helpful variables
-dir_path = os.path.dirname(os.path.realpath(__file__)) # full path to the directory of this script
+dir_path = os.path.dirname(os.path.realpath(__file__))  # full path to the directory of this script
 
 # Modify these to match your particular screen
 width = 800
@@ -78,15 +91,17 @@ lastPlayedFile = os.path.join(tmpDir, 'last_played.txt')
 
 # set the video file information
 video_file = find_video(args, utils.read_file(lastPlayedFile))
-video_name = os.path.splitext(os.path.basename(video_file))[0] # video name, no ext
+video_name = os.path.splitext(os.path.basename(video_file))[0]  # video name, no ext
 
 # setup the logger, log to tmp/log.log
-logging.basicConfig(filename=os.path.join(tmpDir,'log.log'), datefmt='%m/%d %H:%M', format="%(levelname)s %(asctime)s: %(message)s", level=getattr(logging, 'INFO'))
+logging.basicConfig(filename=os.path.join(tmpDir, 'log.log'), datefmt='%m/%d %H:%M',
+                    format="%(levelname)s %(asctime)s: %(message)s",
+                    level=getattr(logging, 'INFO'))
 
 # check if we have a "save" file
 currentPosition = float(args.start)
 saveFile = os.path.join(tmpDir, video_name + '.txt')
-if( os.path.exists(saveFile)):
+if(os.path.exists(saveFile)):
     currentPosition = float(utils.read_file(saveFile))
 
 # setup the screen
@@ -95,7 +110,7 @@ epd = epd7in5_V2.EPD()
 # Initialize the screen
 epd.init()
 
-grabFile = os.path.join(tmpDir,'grab.jpg')
+grabFile = os.path.join(tmpDir, 'grab.jpg')
 
 logging.info('Loading %s' % video_file)
 
@@ -103,7 +118,7 @@ logging.info('Loading %s' % video_file)
 videoInfo = utils.get_video_info(video_file)
 
 if(currentPosition >= videoInfo['frame_count']):
-    currentPosition = args.start # in case we went over the frame count
+    currentPosition = args.start  # in case we went over the frame count
 
 # set the position we want to use
 frame = currentPosition
@@ -121,22 +136,27 @@ if(args.timecode):
     font18 = ImageFont.truetype(os.path.join(dir_path, 'waveshare_lib', 'pic', 'Font.ttc'), 18)
 
     # show the timecode of the video in the format HH:mm:SS
-    message = '%s' % utils.display_time(frame/videoInfo['fps'],3,'{value:02d}',':',True, utils.intervals[2:])
+    message = '%s' % utils.display_time(seconds=frame/videoInfo['fps'],
+                                        granularity=3,
+                                        timeFormat='{value:02d}',
+                                        joiner=':',
+                                        show_zeros=True,
+                                        intervals=utils.intervals[2:])
 
     # get a draw object
     draw = ImageDraw.Draw(pil_im)
-    draw.rectangle((width/2-100,0,width/2+100,20), fill = (0,0,0))
-    tw,th = draw.textsize(message) # gets the width and height of the text drawn
+    draw.rectangle((width/2-100, 0, width/2+100, 20), fill=(0, 0, 0))
+    tw, th = draw.textsize(message)  # gets the width and height of the text drawn
 
     # draw timecode, centering on the middle
-    draw.text(((width-tw)/2,height-20), message, font = font18, fill = (255,255,255))
+    draw.text(((width-tw)/2, height-20), message, font=font18, fill=(255, 255, 255))
 
 # Dither the image into a 1 bit bitmap (Just zeros and ones)
-pil_im = pil_im.convert(mode='1',dither=Image.FLOYDSTEINBERG)
+pil_im = pil_im.convert(mode='1', dither=Image.FLOYDSTEINBERG)
 
 # display the image
 epd.display(epd.getbuffer(pil_im))
-logging.info('Diplaying frame %d (%d seconds) of %s' % (frame,frame/videoInfo['fps'],video_name))
+logging.info('Diplaying frame %d (%d seconds) of %s' % (frame, frame/videoInfo['fps'], video_name))
 
 # save the next position
 currentPosition = currentPosition + float(args.increment)
@@ -153,7 +173,5 @@ if(currentPosition >= videoInfo['frame_count']):
 utils.write_file(saveFile, currentPosition)
 utils.write_file(lastPlayedFile, video_file)
 
-# NB We should run sleep() while the display is resting more often, but there's a bug in the driver that's slightly fiddly to fix. Instead of just sleeping, it completely shuts down SPI communication 
 epd.sleep()
-#epd7in5.epdconfig.module_exit()
 exit()
