dc61f01dbc69ea8fd5475513b8421888a51d09dd
Rob Weber <robweberjr@gmail.com>
2020-10-23 11:09:12 -0500

    used flak8 to make pep8 compatible

--- analyze.py
+++ analyze.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-import argparse, logging, os, time, sys, random, utils
+import argparse
+import logging
+import os
+import utils
 
 # set path to ffmpeg
 os.environ['PATH'] += os.pathsep + '/usr/local/bin/'
+
 
 def time_to_play(total_frames, increment, delay):
     # find out how many frames will display
@@ -20,30 +22,34 @@ def time_to_play(total_frames, increment, delay):
 # parse the arguments
 parser = argparse.ArgumentParser(description='VSMP Settings')
 parser.add_argument('-f', '--file', type=utils.check_mp4, required=True,
-    help="File to analyze")
+                    help="File to analyze")
 parser.add_argument('-d', '--delay',  default=120,
-    help="Delay between screen updates, in seconds")
+                    help="Delay between screen updates, in seconds")
 parser.add_argument('-i', '--increment',  default=4,
-    help="Number of frames skipped between screen updates")
+                    help="Number of frames skipped between screen updates")
 parser.add_argument('-s', '--start', default=1,
-    help="Start at a specific frame")
+                    help="Start at a specific frame")
 
 args = parser.parse_args()
 
-logging.basicConfig(datefmt='%m/%d %H:%M', format="%(asctime)s: %(message)s", level=getattr(logging, 'INFO'))
+logging.basicConfig(datefmt='%m/%d %H:%M', format="%(asctime)s: %(message)s",
+                    level=getattr(logging, 'INFO'))
 
 # run ffmpeg.probe to get the frame rate and frame count
 videoInfo = utils.get_video_info(args.file)
 
 # print some initial information
 print('Analyzing %s' % args.file)
-print('Starting Frame: %s, Frame Increment: %s, Delay between updates: %s' % (args.start, args.increment, args.delay))
-print('Video framerate is %ffps, total video is %f minutes long' % (videoInfo['fps'], videoInfo['runtime']/60))
+print('Starting Frame: %s, Frame Increment: %s, Delay between updates: %s' %
+      (args.start, args.increment, args.delay))
+print('Video framerate is %ffps, total video is %f minutes long' %
+      (videoInfo['fps'], videoInfo['runtime']/60))
 print('')
 
-# setup some helpful variables
-dir_path = os.path.dirname(os.path.realpath(__file__)) # full path to the directory of this script
-video_name = os.path.splitext(os.path.basename(args.file))[0] # video name, no ext
+# full path to the directory of this script
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# video name, no ext
+video_name = os.path.splitext(os.path.basename(args.file))[0]
 
 # create the tmp directory if it doesn't exist
 tmpDir = os.path.join(dir_path, 'tmp')
@@ -52,24 +58,26 @@ if (not os.path.exists(tmpDir)):
 
 # check if we have a "save" file
 currentPosition = float(args.start)
-saveFile = os.path.join(tmpDir,video_name + '.txt')
-if( os.path.exists(saveFile)):
+saveFile = os.path.join(tmpDir, video_name + '.txt')
+if(os.path.exists(saveFile)):
     currentPosition = float(utils.read_file(saveFile))
 
 # find total time to play entire movie
 print('Entire Video:')
-time = time_to_play(videoInfo['frame_count'], float(args.increment), float(args.delay))
+time_to_play(videoInfo['frame_count'],
+             float(args.increment), float(args.delay))
 print('')
 
 
 # find time to play what's left
 print('Remaining Video:')
-time = time_to_play(videoInfo['frame_count'] - currentPosition, float(args.increment), float(args.delay))
+time_to_play(videoInfo['frame_count'] - currentPosition,
+             float(args.increment), float(args.delay))
 print('')
 
 # figure out how many 'real time' minutes per hour
 secondsPerIncrement = float(args.increment)/videoInfo['fps']
-framesPerSecond = secondsPerIncrement/float(args.delay) # this is how many "seconds" of film actually shown per second of realtime
+framesPerSecond = secondsPerIncrement/float(args.delay)  # this is how many "seconds" of film actually shown per second of realtime
 
 minutesPerHour = (framesPerSecond * 60)
 print('Minutes of film displayed breakdown:')
